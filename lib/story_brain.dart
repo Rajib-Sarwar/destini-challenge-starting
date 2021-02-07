23956ded907d0b5040f3e3db2cdf3bc6cfe0311b
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2021-02-07 13:51:45 +0600

    logic to get new story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -53,8 +53,18 @@ class StoryBrain {
   int storyNumber = 0;
 
   void nextStory(int choiceNumber) {
-    if (choiceNumber < _storyData.length - 1) {
-      storyNumber++;
+    if (choiceNumber == 1 && storyNumber == 0) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 0) {
+      storyNumber = 1;
+    } else if (choiceNumber == 1 && storyNumber == 1) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 1) {
+      storyNumber = 3;
+    } else if (choiceNumber == 1 && storyNumber == 2) {
+      storyNumber = 5;
+    } else if (choiceNumber == 2 && storyNumber == 2) {
+      storyNumber = 4;
     }
   }
 
